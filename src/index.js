871713eb97aea7e55f39bdb3f09e240c94731806
pablo <pcoronel@untref.edu.ar>
2018-10-18 17:42:25 -0300

    initialState en config store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import initialState from './store/initialState';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// configuracion del store
+const rootReducer = '';
+function configureStore(initialState) {
+	return createStore(rootReducer, initialState, applyMiddleware(thunk));
+}
+const store = configureStore();
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

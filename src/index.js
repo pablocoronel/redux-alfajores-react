9d09e0fc0a1062f7ba7117df7c42a738641ee6b3
pablo <pcoronel@untref.edu.ar>
2018-10-18 17:49:12 -0300

    agregado reductor general

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import initialState from './store/initialState';
+import rootReducer from './reducers';
 
 // configuracion del store
-const rootReducer = '';
 function configureStore(initialState) {
 	return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }

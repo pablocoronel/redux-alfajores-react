cee95404dcadacda4346525f78efa55bb7fe717d
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-12-15 22:42:04 -0300

    root path vacio

--- src/store/config.js
+++ src/store/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_baseURL = 'http://127.0.0.1:8000/';
-export const router_baseURL = '/';
+export const router_baseURL = '';
 
 const API = axios.create({
 	baseURL: API_baseURL
